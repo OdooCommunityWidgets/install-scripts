f9a2d993a5e8d98734f4f6130d51b92aeb4e7f78
Luke Branch <lukebranch@gmail.com>
2015-04-10 15:12:09 +0800

    Delete odoo_install.sh

--- odoo/8.0/odoo_install.sh
+++
@@ -1,7 +0,0 @@
-# This is a placeholder for an Odoo 8.0 install script
-# Planned features include:
-# Allow for user input of master password for Odoo
-# Allow choice for multi-server instance during install script
-# Reporting Engine boolean options during install: Aeroo, Pentaho, Jasper
-# Virtual hosts boolean options during install: Apache or Nginx
-# # Add domains and port configurations options
