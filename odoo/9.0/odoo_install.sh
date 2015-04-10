292de55a68c8ff95f3cdde8b240f33955c356fb5
Luke Branch <lukebranch@gmail.com>
2015-04-10 15:09:31 +0800

    Create odoo_install.sh

---
+++ odoo/9.0/odoo_install.sh
@@ -0,0 +1,8 @@
+# This is a placeholder for an Odoo 9.0 install script
+# Planned features include:
+# Allow for user input of master password for Odoo
+# Allow choice for multi-server instance during install script
+# Reporting Engine boolean options during install: Aeroo, Pentaho, Jasper
+# Virtual hosts boolean options during install: Apache or Nginx
+# # Add domains and port configurations options
+
