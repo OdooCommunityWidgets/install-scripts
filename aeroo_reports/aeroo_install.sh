58b757b39f9b16bbc72b390df1917915c9e039db
Luke Branch <lukebranch@gmail.com>
2015-05-25 14:39:15 +0800

    Add Description

--- aeroo_reports/aeroo_install.sh
+++ aeroo_reports/aeroo_install.sh
@@ -1,10 +1,44 @@
+# -*- encoding: utf-8 -*-
+################################################################################
+#
+# Copyright (c) 2015 Luke Branch ( https://github.com/odoocommunitywidgets ) 
+#               All Rights Reserved.
+#               General Contact <odoocommunitywidgets@gmail.com>
+#
+# WARNING: This script as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# guarantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# This script is distributed in the hope that it will be useful,
+# but comes WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+################################################################################
+# DESCRIPTION: This script is designed to install all the dependencies for the aeroo-reports modules from Alistek. 
+# If you'd like to install Odoo along with it simply uncomment lines 108-113. Otherwise it assumes you already have Odoo
+# installed according to the script here: 
+# https://raw.githubusercontent.com/lukebranch/odoo-install-scripts/master/odoo-saas4/ubuntu-14-04/odoo_install.sh
+
 # Install Git:
 echo -e "\n---- Install Git ----"
 sudo apt-get install git -y
 
 # Install AerooLib:
 echo -e "\n---- Install AerooLib ----"
-sudo apt-get install python-setuptools python-genshi python-cairo python-lxml libreoffice-script-provider-python libreoffice-base -y
+sudo apt-get install python-setuptools python-genshi python-cairo python-lxml libreoffice-script-provider-python libreoffice-base python-cups -y
 sudo mkdir /opt/aeroo
 cd /opt/aeroo
 sudo git clone https://github.com/aeroo/aeroolib.git
@@ -70,9 +104,9 @@ sudo touch /etc/init.d/office
 sudo python3 /opt/aeroo/aeroo_docs/aeroo-docs start -c /etc/aeroo-docs.conf
 sudo ln -s /opt/aeroo/aeroo_docs/aeroo-docs /etc/init.d/aeroo-docs
 sudo update-rc.d aeroo-docs defaults
-sudo service aeroo-docs start
+sudo service aeroo-docs restart
 
-# If you encounter and error "Unable to lock on the pidfile while trying #16 just restart your server (sudo shutdown -r now)                         and try #16 again after reboot.
+# If you encounter and error "Unable to lock on the pidfile while trying #16 just restart the service (sudo service aeroo-docs restart).
 
 # Install Odoo from Source
 # echo -e "\n---- Install Odoo from Source ----"
@@ -83,7 +117,6 @@ sudo service aeroo-docs start
 
 # Install Aeroo Reports:
 echo -e "\n---- Install Aeroo Reports: ----"
-sudo apt-get install python-cups -y
 cd /opt/odoo/custom
 sudo git clone -b master https://github.com/aeroo/aeroo_reports.git
 
