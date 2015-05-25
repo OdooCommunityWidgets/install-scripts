ef1d090956c97130c58dab6e3b570f91252915bf
Luke Branch <lukebranch@gmail.com>
2015-05-25 17:06:53 +0800

    update to include symlink to ../custom/addons
    added symlink:
    
    sudo ln -s /opt/odoo/custom/aeroo_reports/* /opt/odoo/custom/addons/

--- aeroo_reports/aeroo_install.sh
+++ aeroo_reports/aeroo_install.sh
@@ -134,6 +134,7 @@ while true; do
     case $yn in
         [Yy]* ) cd /opt/odoo/custom
         sudo git clone -b master https://github.com/aeroo/aeroo_reports.git
+        sudo ln -s /opt/odoo/custom/aeroo_reports/* /opt/odoo/custom/addons/
         break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
